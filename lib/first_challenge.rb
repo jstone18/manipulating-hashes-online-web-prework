f314a25abc135705bc4c2707fb03819f499bf6a2
Jason Stone <jason.stone0808@gmail.com>
2018-11-18 21:56:15 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,8 +13,7 @@ def first_challenge
     }
   }
 
-  no_strawberry = ["cookie dough", "mint chip"]
-  contacts["Freddy Mercury"][:favorite_icecream_flavors] = no_strawberry
+  
 
 
   #remember to return your newly altered contacts hash!
